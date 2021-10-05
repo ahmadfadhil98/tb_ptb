244cd2de5776c6b00f51e30398dd80667f83cd60
ahmadfadhil98 <1611522009@student.unand.ac.id>
2021-10-05 19:17:06 +0700

    first commit

---
+++ app/src/main/java/com/example/aplikasiptb/LaunchScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.aplikasiptb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LaunchScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_screen);
+    }
+}
